426ccbe10be23500b5974eff244502caa799384d
刘裕勇 <61225165+Liuyuyong0414@users.noreply.github.com>
2020-05-27 16:25:16 +0800

    Add files via upload
    【问题描述】请编写一个函数，实现两个复数相减。并编写main函数调用该函数。
    【输入形式】输入包括4个浮点数，分别是第一个数的实部和虚部，以及第二个数的部和虚部，每个数字之间有一个空格
    
    【输出形式】两个数，分别是差的实部和虚部，保留2位小数，中间用1个空格空开
    
    【样例输入】
    
    3 2 6 5
    【样例输出】
    -3.00 -3.00
    
    【样例输入】
    
    90.5 -1 30 -0.8 
    【样例输出】
    60.50 -0.20

---
+++ CG (1).cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float a,b,c,d,x,y;
+	cin>>a>>b>>c>>d;
+	float fushu(float i,float j);
+	x=fushu(a,c);
+	y=fushu(b,d);
+	cout<<fixed<<setprecision(2)<<x<<' '<<y;
+}
+float fushu(float i,float j)
+{
+	return (i-j);
+}
+
