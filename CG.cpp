844abb382ed549ed37d4b07f4c506dda72272afe
刘裕勇 <61225165+Liuyuyong0414@users.noreply.github.com>
2020-05-27 16:21:26 +0800

    Add files via upload
    【问题描述】从键盘输入两组英文字符（包括大写和小写英文字符），每组英文字母中都没有重复字母，分别转化为小写字符并存入两个数组a和b中，以英文字母表为依据，请输出a数组和b数组中在英文字母表中最接近的两个英文字母的距离。
    【输入形式】输入包括两行英文字母，有大写和小写字母，每行不超过10个元素
    【输出形式】输出一个整数，表示两个数组中最接近的两个英文字母的距离，如果两个字母相同，则输出为0
    【样例输入】
    
    abxy
    
    cAzzp
    
    【样例输出】
    0
    
    【样例输入】
    
    aBc
    
    deF
    
    【样例输出】
    1
    
    【样例输入】
    
    xYzy
    
    deFac
    
    【样例输出】
    18

---
+++ CG.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string a,b;
+	int c[50],i,j,n1,n2,x=0,min=100;
+	for(i=0;i<50;i++)
+	a[i]=100;
+	getline(cin, a);
+	getline(cin, b);
+	for(i=0;a[i];i++)
+	{
+		if(a[i]<='Z')a[i]+=32;
+	}
+	for(j=0;b[j];j++)
+	{
+		if(b[j]<='Z')b[j]+=32;
+	}
+	for(n1=0;n1<i;n1++)
+	for(n2=0;n2<j;n2++)
+	{
+		c[x++]=a[n1]-b[n2];
+		if(c[x-1]<0)c[x-1]=-c[x-1];
+	}
+	for(i=0;i<x;i++)
+	if(c[i]<min)min=c[i];
+	cout<<min;
+}
+
